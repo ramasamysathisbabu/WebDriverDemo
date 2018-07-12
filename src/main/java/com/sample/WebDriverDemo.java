3ffff305b1ee3c07b2a3d44739798871c9cb040f
ramasamysathisbabu <39998708+ramasamysathisbabu@users.noreply.github.com>
2018-07-12 17:05:56 -0400

    Radio button selection using Selenium automation

--- src/main/java/com/sample/WebDriverDemo.java
+++ src/main/java/com/sample/WebDriverDemo.java
@@ -3,17 +3,31 @@ package com.sample;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("webdriver.gecko.driver", "C:\\dev\\tools\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+//		System.setProperty("webdriver.gecko.driver", "C:\\dev\\tools\\geckodriver.exe");
+//		WebDriver driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\dev\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://google.com");
 		WebElement searhField = driver.findElement(By.name("q"));
-		searhField.sendKeys("java");
+		searhField.sendKeys("pluralsight");
+		searhField.submit();
+		
+		
+		WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
+		Thread.sleep(5000);
+		imagesLink.click();
+		
+		WebElement imageElement = driver.findElements(By.cssSelector("a[class = rg_l]")).get(0);
+		imageElement.click();
+		
 	}
 
 }
---
+++ src/main/java/com/sample/WebDriverRadioButtons.java
@@ -0,0 +1,21 @@
+package com.sample;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverRadioButtons {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\dev\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/workspaces_STS/WebDriverDemo/src/main/webapp/RadioButtonTest.html");
+		
+		List<WebElement> radioButtons = driver.findElements(By.name("color"));
+		radioButtons.get(1).click();
+	}
+
+}
